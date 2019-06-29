f5814dc89158cee86e49dd649c50b75ddb285bac
doxuandungx10 <doxuandungx10@gmail.com>
2019-06-30 01:26:40 +0700

    Initial commit

---
+++ nghich.js
@@ -0,0 +1,28 @@
+let answer = prompt("Cậu thấy Dũng có đẹp trai không? :)").toLowerCase();
+if ( answer === "có" ){
+    confirm(" Ừ đúng thế :) ")
+}
+else if ( answer === "đéo" ){
+    confirm(" Occho :) Dũng đẹp trai ");
+}
+else if ( answer === "ko" ){
+    confirm(" Occho :) Dũng đẹp trai ");
+    
+}
+else if ( answer === "không" ){
+    confirm(" Occho :) Dũng đẹp trai ");
+
+}
+else if ( answer === "lồn" ){
+    confirm(" Đm cậu không nói tục :) ");
+}
+else if ( answer === "cặc" ){
+    confirm(" Đm cậu không nói tục :) ");
+}
+else if ( answer === "đmm" ){
+    confirm(" Đm cậu không nói tục :) ");
+}
+else {
+    confirm("Cậu nói gì tớ không hiểu :) Human Language pleaseee :) ")
+}
+
